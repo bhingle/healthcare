a3fabcdc90cdbab4fe82cb2169419c2aa23af80a
Viresh <vireshfegade@gmail.com>
2021-03-28 20:00:28 +0530

    added basic features

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:my_app/page/HospitalInfo.dart';
+import 'package:my_app/page/Stories.dart';
 import 'package:my_app/page/sign_up_page.dart';
 import 'package:my_app/provider/dbdata.dart';
 import 'package:my_app/page/UserType.dart';
